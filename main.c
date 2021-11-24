0ac1f902389582d3f349612a5230da7f0416e788
n01113316 <93564471+n01113316@users.noreply.github.com>
2021-11-23 19:05:18 -0500

    Add files via upload
    added flags for + and # (steven)

--- main.c
+++ main.c
@@ -17,6 +17,8 @@ int main( int argc, char* argv[]){
 	char objCode[SIZE] = {0};
 	char hexString[SIZE*2]={0};
 	char programStartName[7];
+	int plusfinder[5000];
+	int tagfinder[5000];
 	int counter = 0;
 	int startCounter = 0;
 	int programLength = 0;
@@ -26,6 +28,22 @@ int main( int argc, char* argv[]){
 	int currentIndex = 0;
 	int startFlag = 0;
 	int endFlag = 0;
+
+
+	//2 Byte Register Numbers //-Ethan Coco
+	/*
+	int axe = 0;
+	int xxe = 1;
+	int lxe = 2;
+	int bxe = 3;
+	int sxe = 4;
+	int txe = 5;
+	int fxe = 6;
+	int pcxe = 8;
+	int swxe = 9;
+	*/
+	char *optok;//-Ethan Coco
+	char *optok2;//-Ethan Coco
 
 
 
@@ -64,6 +82,11 @@ int main( int argc, char* argv[]){
 	memset(operandCPY, '\0', SIZE * sizeof(char) );
 	oc1 = malloc(SIZE * sizeof(char) );//-
         memset(oc1, '\0', SIZE * sizeof(char) );//-
+
+	optok = malloc(SIZE * sizeof(char));//-Ethan Coco
+	memset(optok,'\0',SIZE * sizeof(char));//-Ethan Coco
+	optok2 = malloc(SIZE * sizeof(char));//-Ethan Coco
+	memset(optok2,'\0',SIZE * sizeof(char));//-Ethan Coco
         //First pass
         while(  fgets( line , SIZE , fp ) != NULL   ) {
 		if( counter >= 32768){
@@ -89,6 +112,20 @@ int main( int argc, char* argv[]){
                                 printf("ERROR: SYMBOL & OPCODE/DIRECTIVE ARE EMPTY.");
                                 exit(0);
                         }
+			//->steven dear
+			if(newsym[0]==43){
+                                newsym[0] = '\n';
+                                memmove(newsym,newsym+1,strlen(newsym));
+                                printf("+ has been removed for %s\n", newsym);
+                                plusfinder[counter]=1;
+                        }
+			if(nexttoken[0]==35){
+				nexttoken[0] = '\n';
+                                memmove(nexttoken,nexttoken+1,strlen(nexttoken));
+                                printf("# has been removed for %s\n", nexttoken);
+				tagfinder[counter]=1;
+			}
+			//<-end
 
                         if ( IsAValidSymbol(SymbolHashTable, newsym) == 0 ) {
 
@@ -161,7 +198,7 @@ int main( int argc, char* argv[]){
 	if(startFlag != 1 || endFlag != 1){
 		printf("ERROR. NONE/MORE THAN ONE START OR END DIRECTIVE PRESENT.\n");
 		exit(0);
-	} 
+	}
 
 	rewind(fp); //set file pointer to beginning of file
 	programLength = counter - startCounter; //calculates length of the program
@@ -184,6 +221,20 @@ int main( int argc, char* argv[]){
                 	nexttoken = strtok( NULL, " \t\n"  ); //token 2
                 	operand = strtok( NULL, "\t\n" ); //token 3
 
+			//->steven dear
+			if(newsym[0]==43){
+                                newsym[0] = '\n';
+                                memmove(newsym,newsym+1,strlen(newsym));
+                        //        printf("+ has been removed for %s\n", newsym);
+                        //        plusfinder[counter]=1;
+                        }
+			if(nexttoken[0]==35){
+                                nexttoken[0] = '\n';
+                                memmove(nexttoken,nexttoken+1,strlen(nexttoken));
+                        //        printf("# has been removed for %s\n", nexttoken);
+                        //        tagfinder[counter]=1;
+			}
+			//<-end
 			currentIndex = SearchSymTab(SymbolHashTable, newsym); //Check if first token is a symbol
 			if(currentIndex != -1){ //Symbol has been found
 			}
@@ -193,6 +244,13 @@ int main( int argc, char* argv[]){
 				strcat(objCode, "0000");
 				insertTRecord(TextRecords, counter, OBJ_LENGTH, objCode);
 				counter +=3;
+				continue;
+			}
+			else if(onebXE(newsym)==1){//checks xe instructions if no symbol is present //-Ethan Coco
+				opTableIndex = IsAnOpcode(newsym);
+				strcpy(objCode,OpcodeTable[opTableIndex].OpCode);
+				insertTRecord(TextRecords,counter,OBJ_LENGTH,objCode);
+				counter +=1;
 				continue;
 			}
 			else{	//if first token is not a symbol or RSUB opcode, rearrange token variables for consistency
@@ -243,12 +301,152 @@ int main( int argc, char* argv[]){
 				default :
 				break;
 			}
+			if(onebXE(nexttoken)!=0){//checks xe instructions if symbol is present //-Ethan Coco
+				if(onebXE(nexttoken)==1){//-Ethan Coco
+					opTableIndex = IsAnOpcode(nexttoken);
+					strcpy(objCode,OpcodeTable[opTableIndex].OpCode);
+					insertTRecord(TextRecords,counter,OBJ_LENGTH,objCode);
+					counter +=1;
+				}else if(onebXE(nexttoken)==2){//-Ethan Coco
+					opTableIndex = IsAnOpcode(nexttoken);
+					strcpy(objCode,OpcodeTable[opTableIndex].OpCode);
+					if(strcmp(nexttoken,"CLEAR")==0 || strcmp(nexttoken,"TIXR")==0){
+						if(strcmp(operand,"A")==0){
+							strcat(objCode,"0");
+						}else if(strcmp(operand,"X")==0){
+							strcat(objCode,"1");
+						}else if(strcmp(operand,"L")==0){
+							strcat(objCode,"2");
+						}else if(strcmp(operand,"B")==0){
+							strcat(objCode,"3");
+						}else if(strcmp(operand,"S")==0){
+							strcat(objCode,"4");
+						}else if(strcmp(operand,"T")==0){
+							strcat(objCode,"5");
+						}else if(strcmp(operand,"F")==0){
+							strcat(objCode,"6");
+						}else if(strcmp(operand,"PC")==0){
+							strcat(objCode,"8");
+						}else if(strcmp(operand,"SW")==0){
+							strcat(objCode,"9");
+						}
+						strcat(objCode,"0");
+					}else if(strcmp(nexttoken,"SHIFTL")==0 || strcmp(nexttoken,"SHIFTR")==0){
+						optok = strtok(operand,",");
+						optok2 = strtok(NULL,"\t\n");
+
+						if(strcmp(optok,"A")==0){
+							strcat(objCode,"0");
+						}else if(strcmp(optok,"X")==0){
+							strcat(objCode,"1");
+						}else if(strcmp(optok,"L")==0){
+							strcat(objCode,"2");
+						}else if(strcmp(optok,"B")==0){
+							strcat(objCode,"3");
+						}else if(strcmp(optok,"S")==0){
+							strcat(objCode,"4");
+						}else if(strcmp(optok,"T")==0){
+							strcat(objCode,"5");
+						}else if(strcmp(optok,"F")==0){
+							strcat(objCode,"6");
+						}else if(strcmp(optok,"PC")==0){
+							strcat(objCode,"8");
+						}else if(strcmp(optok,"SW")==0){
+							strcat(objCode,"9");
+						}
+						strcat(objCode,optok2);
+					}else if(strcmp(nexttoken,"SVC")==0){
+						if(strcmp(operand,"A")==0){
+							strcat(objCode,"0");
+						}else if(strcmp(operand,"X")==0){
+							strcat(objCode,"1");
+						}else if(strcmp(operand,"L")==0){
+							strcat(objCode,"2");
+						}else if(strcmp(operand,"B")==0){
+							strcat(objCode,"3");
+						}else if(strcmp(operand,"S")==0){
+							strcat(objCode,"4");
+						}else if(strcmp(operand,"T")==0){
+							strcat(objCode,"5");
+						}else if(strcmp(operand,"F")==0){
+							strcat(objCode,"6");
+						}else if(strcmp(operand,"PC")==0){
+							strcat(objCode,"8");
+						}else if(strcmp(operand,"SW")==0){
+							strcat(objCode,"9");
+						}
+						strcat(objCode,"0");
+					}else{
+						optok = strtok(operand,",");
+						optok2 = strtok(NULL,"\t\n");
+
+						if(strcmp(optok,"A")==0){
+							strcat(objCode,"0");
+						}else if(strcmp(optok,"X")==0){
+							strcat(objCode,"1");
+						}else if(strcmp(optok,"L")==0){
+							strcat(objCode,"2");
+						}else if(strcmp(optok,"B")==0){
+							strcat(objCode,"3");
+						}else if(strcmp(optok,"S")==0){
+							strcat(objCode,"4");
+						}else if(strcmp(optok,"T")==0){
+							strcat(objCode,"5");
+						}else if(strcmp(optok,"F")==0){
+							strcat(objCode,"6");
+						}else if(strcmp(optok,"PC")==0){
+							strcat(objCode,"8");
+						}else if(strcmp(optok,"SW")==0){
+							strcat(objCode,"9");
+						}
+
+						if(strcmp(optok2,"A")==0){
+							strcat(objCode,"0");
+						}else if(strcmp(optok2,"X")==0){
+							strcat(objCode,"1");
+						}else if(strcmp(optok2,"L")==0){
+							strcat(objCode,"2");
+						}else if(strcmp(optok2,"B")==0){
+							strcat(objCode,"3");
+						}else if(strcmp(optok2,"S")==0){
+							strcat(objCode,"4");
+						}else if(strcmp(optok2,"T")==0){
+							strcat(objCode,"5");
+						}else if(strcmp(optok2,"F")==0){
+							strcat(objCode,"6");
+						}else if(strcmp(optok2,"PC")==0){
+							strcat(objCode,"8");
+						}else if(strcmp(optok2,"SW")==0){
+							strcat(objCode,"9");
+						}
+					}
+					insertTRecord(TextRecords,counter,OBJ_LENGTH,objCode);
+					counter +=2;
+
+				}else if(onebXE(nexttoken)==3){//-Ethan Coco
+					opTableIndex = IsAnOpcode(nexttoken); //Find opcode within OpCodeTable
+					strcpy(objCode, OpcodeTable[opTableIndex].OpCode); //copy opcode hex
+					strcpy(operandCPY, operand); //copy third token to avoid unintentional modification of original
+					sprintf(hexString,"%X", SymbolHashTable[SearchSymTab(SymbolHashTable, operand)]->address); //find referenced symbol, pull the address, convert address to hex, output to "hexString" variable
+					strcat(objCode,hexString);
+					if(newsym == NULL){
+						counter += 3;
+						insertTRecord(TextRecords,counter,OBJ_LENGTH,objCode);
+						//MRecord
+					}else{
+						counter = SymbolHashTable[currentIndex]->address;
+						insertTRecord(TextRecords,counter,OBJ_LENGTH,objCode);
+						//MRecord
+					}
+				}
+				continue;
+			}else{
 			opTableIndex = IsAnOpcode(nexttoken); //Find opcode within OpCodeTable
 			strcpy(objCode, OpcodeTable[opTableIndex].OpCode); //copy opcode hex
 			strcpy(operandCPY, operand); //copy third token to avoid unintentional modification of original
-
+			}
 			//copy opcode
-			strcpy(oc1,objCode);//-
+			//strcpy(oc1,objCode);//-
 
 			//check for indirect addressing
 			if(strstr(operandCPY, ",X") != NULL){
@@ -269,47 +467,23 @@ int main( int argc, char* argv[]){
 			}
 			strcat(objCode,hexString); // add hexstring result to end of objCode
 
-			if(newsym == NULL){ //If no symbols are present
-				//checks to see if the instruction is a 1-byte instruction
-				if(onebXE(oc1)==1){//->
-					counter += 1;
-//					printf("this is one BYTE -> %s\n",nexttoken);
-					insertTRecord(TextRecords, counter, OBJ_LENGTH, oc1);
-                                	insertMRecord(ModRecords, counter, strlen(hexString), programStartName);
-				}else{//<-
-					counter += 3;
-					insertTRecord(TextRecords, counter, OBJ_LENGTH, objCode);
-					insertMRecord(ModRecords, counter, strlen(hexString), programStartName);
-				}
+			if(newsym == NULL){
+				counter += 3;
+				insertTRecord(TextRecords,counter,OBJ_LENGTH,objCode);
+				//MRecord
+			}else{
+				counter = SymbolHashTable[currentIndex]->address;
+				insertTRecord(TextRecords,counter,OBJ_LENGTH,objCode);
+				//MRecord
 			}
-			else{
-				//checks to see if the instruction is a 1-byte instruction
-				if(onebXE(oc1)==1){//->
-					counter = SymbolHashTable[currentIndex]->address;
-//                                      printf("this is one BYTE -> %s\n",nexttoken);
-                                        insertTRecord(TextRecords, counter, OBJ_LENGTH, oc1);
-                                        insertMRecord(ModRecords, counter, strlen(hexString), programStartName);
-                                }else{//<-
-					counter = SymbolHashTable[currentIndex]->address;
-					insertTRecord(TextRecords, counter, OBJ_LENGTH, objCode);
-					insertMRecord(ModRecords, counter, strlen(hexString), programStartName);
-				}
-			}
-		}
+		//}
 	}
+}
 
-	//Write records to filename.obj
-	fprintf(objFP, "H%s\t%06X%06X\n", header.name, header.startAddress, header.length);
-	for(int i=0; i<SIZE; i++){
+	for(int i=0; i<SIZE; i++){//Prints only object code for the time being, will change //-Ethan Coco
 		if(TextRecords[i])
-			fprintf(objFP, "T%06X%02X%s\n", TextRecords[i]->startAddress, TextRecords[i]->length, TextRecords[i]->objectCode);
+			fprintf(objFP,"%s\n",TextRecords[i]->objectCode);
 	}
-
-	for(int i=0; i<SIZE; i++){
-		if(ModRecords[i])
-			fprintf(objFP, "M%06X%02X+%s\n", ModRecords[i]->startAddress, ModRecords[i]->length, ModRecords[i]->symbolName);
-	}
-	fprintf(objFP, "E%06X\n", endRecordAddress);
 
 	//Closing statements
 	fclose( fp );
@@ -330,26 +504,15 @@ int validHex(char *test){ //check if string is a valid hex string
 	return 1;
 }
 
-//function for checking if an opcode belongs to a 1-byte instruction
-int onebXE(char *objCode){//->
-	int bv=0;
-	if(strcmp(objCode,"C4")==0){
-		bv=1;
+//function for checking if an opcode belongs to a 1-byte instruction //-Ethan Coco
+int onebXE(char *objCode){
+	int check = 0;
+	if(strcmp(objCode,"FIX")==0 || strcmp(objCode,"FLOAT")==0 || strcmp(objCode,"HIO")==0 || strcmp(objCode,"NORM")==0 || strcmp(objCode,"SIO")==0 || strcmp(objCode,"TIO")==0){
+		check = 1;
+	}else if(strcmp(objCode,"ADDR")==0 || strcmp(objCode,"CLEAR")==0 || strcmp(objCode,"COMPR")==0 || strcmp(objCode,"DIVR")==0 || strcmp(objCode,"MULR")==0 || strcmp(objCode,"RMO")==0 || strcmp(objCode,"SHIFTL")==0 || strcmp(objCode,"SHIFTR")==0 || strcmp(objCode,"SUBR")==0 || strcmp(objCode,"SVC")==0 || strcmp(objCode,"TIXR")==0){
+		check = 2;
+	}else if(strcmp(objCode,"ADDF")==0 || strcmp(objCode,"COMPF")==0 || strcmp(objCode,"DIVF")==0 || strcmp(objCode,"LDB")==0 || strcmp(objCode,"LDF")==0 || strcmp(objCode,"LDS")==0 || strcmp(objCode,"LDT")==0 || strcmp(objCode,"LPS")==0 || strcmp(objCode,"MULF")==0 || strcmp(objCode,"SSK")==0 || strcmp(objCode,"STB")==0 || strcmp(objCode,"STF")==0 || strcmp(objCode,"STI")==0 || strcmp(objCode,"STS")==0 || strcmp(objCode,"STT")==0 || strcmp(objCode,"SUBF")==0){
+		check = 3;//
 	}
-	else if(strcmp(objCode,"C0")==0){
-		bv=1;
-	}
-	else if(strcmp(objCode,"F4")==0){
-                bv=1;
-        }
-	else if(strcmp(objCode,"C8")==0){
-                bv=1;
-        }
-	else if(strcmp(objCode,"FO")==0){
-                bv=1;
-        }
-	else if(strcmp(objCode,"F8")==0){
-                bv=1;
-        }
-	return bv;
-}//<-
+	return check;
+}
